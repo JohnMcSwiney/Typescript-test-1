d1288520e07e4fa5a8c3f2cfb255cf21f932866c
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2024-01-14 12:58:04 -0700

    initial upload, working at base running ts

---
+++ react-ts-test-1/src/App.tsx
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Running using a Typescript file
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
